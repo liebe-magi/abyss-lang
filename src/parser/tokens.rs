d1ade715d564ba31cf5305e986779646110b8718
りーべ <liebe.magi@gmail.com>
2025-11-08 19:32:30 +0900

    Refactor escape sequence handling in tokens.rs

--- src/parser/tokens.rs
+++ src/parser/tokens.rs
@@ -159,16 +159,14 @@ pub fn lexer() -> impl Parser<char, Vec<SpannedToken>, Error = Simple<char>> {
         Token::Arcana(number.parse::<i64>().unwrap())
     });
 
-    let escape = just('\\').ignore_then(
-        one_of(r#""ntr\"#).map(|c| match c {
-            '"' => '"',
-            'n' => '\n',
-            't' => '\t',
-            'r' => '\r',
-            '\\' => '\\',
-            other => other, // fallback: just use the char as is
-        })
-    );
+    let escape = just('\\').ignore_then(one_of(r#""ntr\"#).map(|c| match c {
+        '"' => '"',
+        'n' => '\n',
+        't' => '\t',
+        'r' => '\r',
+        '\\' => '\\',
+        other => other, // fallback: just use the char as is
+    }));
     let rune_char = escape.or(filter(|c| *c != '"' && *c != '\\'));
     let rune = just('"')
         .ignore_then(rune_char.repeated().collect::<String>())
